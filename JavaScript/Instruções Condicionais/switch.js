4f89d0f59e23525055b790330ca3a4800437f555
GustavoRSL <d7gustavo@yahoo.com.br>
2023-03-21 16:31:54 -0300

    Adicionando Fundamentos JS

---
+++ JavaScript/Instruções Condicionais/switch.js
@@ -0,0 +1,22 @@
+
+function funcao(arg){
+    switch(arg){
+        case 1:
+            console.log("Arg é 1");
+            break;
+        case 2:
+            console.log("Arg é 2");
+            break;
+        case 3:
+            console.log("Arg é 3");
+            break;
+        default:
+            console.log("Arg é algum outro valor diferente de 1,2 e 3.");
+            break;
+    }
+}
+
+funcao(1);
+funcao(2);
+funcao(3);
+funcao(4);
---
+++ JavaScript/Operadores/operador_modulo.js
@@ -0,0 +1,8 @@
+var x = 11;
+
+if(x % 2 == 0){
+    console.log('X é par');
+}
+else {
+    console.log('X é impar');
+}
